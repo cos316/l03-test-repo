d03bbabe3debd2db70340b15689e25c52d65da2f
Leon Schuermann <leon@is.currently.online>
2023-09-12 08:29:19 -0400

    Print current weekday

--- main.go
+++ main.go
@@ -10,5 +10,9 @@ func main() {
 	fmt.Println("Hello COS316!")
 
 	assignment_due := utils.AssignmentDueToday(time.Now())
-	fmt.Println("Is there an assignment due today?", assignment_due)
+	fmt.Printf(
+		"Today is a %s. Is there an assignment due today? %t\n",
+		time.Now().Weekday(),
+		assignment_due,
+	)
 }
