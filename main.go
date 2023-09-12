7220417b5559958adfe908b0e72a4e0ab09b680b
Leon Schuermann <leon@is.currently.online>
2023-09-12 08:41:55 -0400

    Soften language for assignment due-date calculation
    Not every Wednesday necessarily has an assignment due.

--- main.go
+++ main.go
@@ -10,5 +10,5 @@ func main() {
 	fmt.Println("Hello COS316!")
 
 	assignment_due := utils.AssignmentDueToday(time.Now())
-	fmt.Println("Is there an assignment due today?", assignment_due)
+	fmt.Println("Could there be an assignment due today?", assignment_due)
 }
