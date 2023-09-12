9df743e007029e2024e8a2e209bf57a213246b95
Leon Schuermann <leon@is.currently.online>
2023-09-12 08:38:44 -0400

    Check semester date-bounds for assignment-due calucation

--- utils/assignments.go
+++ utils/assignments.go
@@ -5,6 +5,13 @@ import (
 )
 
 func AssignmentDueToday(t time.Time) bool {
+	start, _ := time.Parse(time.RFC822, "05 Sep 23 00:00 ET")
+	end, _ := time.Parse(time.RFC822, "16 Dec 23 00:00 ET")
+
+	if t.Before(start) || t.After(end) {
+		return false
+	}
+
 	return t.Weekday() == time.Wednesday
 }
 
